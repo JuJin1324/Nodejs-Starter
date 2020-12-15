bde30f31151217a639c8d1b9e9f16ff00ea4684e
jujin1324 <jujin1324@daum.net>
2020-12-15 23:49:42 +0900

    Chapter14# init proj

---
+++ chapter14/index_cluster.js
@@ -0,0 +1,28 @@
+const winston = require('./config/winston');
+const cluster = require('cluster');
+
+const startWorker = () => {
+    let worker = cluster.fork();
+    winston.info(`CLUSTER: Worker ${worker.id} started`);
+}
+
+if (cluster.isMaster) {
+    // require('os').cpus().forEach(() => {
+    //     startWorker();
+    // });
+    // [0, 1].forEach(() => {
+    //     startWorker();
+    // })
+    startWorker();
+
+    /* disconnect -> exit */
+    cluster.on('disconnect', worker => {
+        winston.info(`CLUSTER: Worker ${worker.id} disconnected from the cluster.`);
+    });
+    cluster.on('exit', (worker, code, signal) => {
+        winston.info(`CLUSTER Worker ${worker.id} died with exit code ${code} (${signal})`);
+        startWorker();
+    });
+} else {    /* It's not the Master then start server */
+    require('./index')();
+}
